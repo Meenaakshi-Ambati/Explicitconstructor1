d542dc203ce89d269a10899e9e39746a38dc4a0b
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-11-11 13:11:37 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExplicitInterface1
+{
+   interface I1
+   {
+       void InterfaceMethod();
+   }
+
+   
+   interface I2
+   {
+       void InterfaceMethod();
+   }
+
+   class MyClass : I1, I2
+   {
+      void I1.InterfaceMethod()
+      {
+          Console.WriteLine("I1 Interface Method");
+
+      }
+
+      void I2.InterfaceMethod()
+      {
+          Console.WriteLine("I2 Interface Method");
+          
+      }
+   }
+
+   class Program
+   {
+       public static void Main(string[] args)
+       {
+           I1 i1 = new MyClass();
+           I2 i2 = new MyClass();
+
+           i1.InterfaceMethod();
+           i2.InterfaceMethod();
+       }
+   }
+}
